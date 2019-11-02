6c31c411e547b811274886caf88d74f5fc41f207
Karen-Mtz <dkareen.mtz@gmail.com>
2019-11-02 13:43:32 -0600

    primer avance

---
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Apple Device 18 on 11/2/19.
+//  Copyright © 2019 Tonipocket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
